e5643abe6fb5794ce5849bec45273903277413e6
Almaz <almaz.kzn2005@yandex.ru>
2022-01-05 21:49:55 +0300

    Добавлен отскок шарика

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 import os
 import sys
@@ -10,6 +12,9 @@ size = width, height = 570, 400
 screen = pygame.display.set_mode(size)
 fps = 60
 
+# переменная для предотвращения многократного отскока
+st_y1 = 0
+
 
 def load_image(name):  # load image
     fullname = os.path.join('data', name)
@@ -21,21 +26,58 @@ def load_image(name):  # load image
 
 
 # создание подвижной платформы-doska и шарика
+
 doska_w = 100
 doska_h = 20
 doska_speed = 10
 doska = pygame.Rect(250, 360, doska_w, doska_h)
 
 ball_radius = 10
-ball_speed = 6
+ball_speed = 3
 ball_rect = int(ball_radius * 2 ** 0.5)
 ball = pygame.Rect(290, 335, ball_rect, ball_rect)
+
 # направления движения шарика по осям x,y
 dx = 1
 dy = -1
 # создание блоков
 blocks = [pygame.Rect(10 + 70 * i, 10 + 50 * j, 60, 40) for i in range(8) for j in range(5)]
 colors = [(randrange(30, 255), randrange(30, 255), randrange(30, 255)) for i in range(8) for j in range(5)]
+
+
+def ball_update():
+    ball.x += ball_speed * dx
+    ball.y += ball_speed * dy
+
+
+def collide():
+    # отскок шарика от стенок
+    global dx, dy, st_y1
+
+    if ball.x >= 554 or ball.x <= 8:
+        dx = -dx
+
+    if ball.y < 5 or ball.y >= 378:
+        dy = -dy
+
+    # отскок шарика от платформы
+    if ball.y <= st_y1 or st_y1 == 0:
+        st_y1 = -1
+
+    if pygame.Rect.colliderect(ball, doska) and st_y1 == -1:
+        dy = -dy
+        a = random.randint(0, 1)
+
+        if a and ball.x > 7:
+            dx = -dx
+
+        st_y1 = ball.y - 50
+
+    # отскок от плиток
+    for block in blocks:
+        if pygame.Rect.colliderect(block, ball):
+            dy = -dy
+
 
 if __name__ == '__main__':
     running = True
@@ -54,8 +96,11 @@ if __name__ == '__main__':
         pygame.draw.circle(screen, 'white', ball.center, ball_radius)
 
         # движение шарика
-        ball.x += ball_speed * dx
-        ball.x += ball_speed * dy
+        ball_update()
+
+        # отскок шарика
+        collide()
+
         # управление
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT] and doska.left > 0:
